f03732de5140a99afb4bda659a82ed6c1f992a4b
yan-zhuang2 <jeffcorn4books@gmail.com>
2023-10-22 12:00:58 +0200

    up

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -5,44 +5,88 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // 
     private Rigidbody2D rb;
     private Animator anim;
     private SpriteRenderer sprite;
-    private int wholeNumber = 16;
-
-    private float decimalNumber = 4.54f;
-
-    private string text = "blabla";
-
-    private bool boolean = false;
-    private bool doubleJump;
-    private float dirX=0f;
+    private TrailRenderer trailRenderer;
     
-    // for ground check
-    [SerializeField] private LayerMask jumpableGround;
+    // 
+    private int wholeNumber = 16;
+    private float decimalNumber = 4.54f;
+    private string text = "blabla";
+    private bool boolean = false;
+    private float dirX = 0f;
+    private float dirY = 0f;
+    
+    // move & jump speed
     private float moveSpeed = 7f;
-     private float jumpSpeed = 14f;
-     private BoxCollider2D coll;
-    // Start is called before the first frame update
+    private float jumpSpeed = 14f;
+    
+    // ground check
+    private BoxCollider2D coll;
+    [SerializeField] private LayerMask jumpableGround;
+    
+    // doubleJump;
+    private bool doubleJump;
+    
+    // dash
+    private float dashingVelocity = 14f;
+    private float dashingTime = .5f;
+
+    private Vector2 dashingDir;
+    private bool isDashing;
+    private bool canDash=true;
+    // 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
         coll = GetComponent<BoxCollider2D>();
+        trailRenderer = GetComponent<TrailRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsGrounded() && !Input.GetButton("Jump"))
+        var dashInput = Input.GetButtonDown("Dash");
+        var jumpDownInput = Input.GetButtonDown("Jump");
+        var jumpInput = Input.GetButton("Jump");
+        // getAxisRaw: make the velocity 0 immediately 
+        dirX = Input.GetAxisRaw("Horizontal");
+        dirY = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+        if (dashInput && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+            trailRenderer.emitting = true;
+            dashingDir = dashingDir = new Vector2(dirX, dirY);
+            if (dashingDir == Vector2.zero)
+            {
+                dashingDir = new Vector2(transform.localScale.x, 0);
+            }
+            // add stopping dash
+            StartCoroutine(StopDashing());
+        }
+        
+        anim.SetBool("isDashing", isDashing);
+        if (isDashing)
+        {
+            rb.velocity = dashingDir.normalized * dashingVelocity;
+            return;
+        }
+        if (IsGrounded())
+        {
+            canDash = true;
+        }
+        // update double jump status
+        if (IsGrounded() && !jumpInput)
         {
             doubleJump = false;
         }
-        // getAxisRaw: make the velocity 0 immediately 
-        dirX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirX * moveSpeed,rb.velocity.y);
-        if (Input.GetButtonDown("Jump"))
+        if (jumpDownInput)
         {
             if (IsGrounded() || doubleJump)
             {
@@ -51,26 +95,34 @@ public class PlayerMovement : MonoBehaviour
             }
         }
         UpdateAnim();
-        
     }
 
+    // update the running condition
     void UpdateAnim()
     {
         if (dirX > 0f)
         {
-            anim.SetBool("running",true);
+            anim.SetBool("running", true);
             sprite.flipX = false;
         }
-        else if(dirX<0f)
+        else if (dirX < 0f)
         {
-            anim.SetBool("running",true);
+            anim.SetBool("running", true);
             sprite.flipX = true;
         }
         else
         {
-            anim.SetBool("running",false); 
+            anim.SetBool("running", false);
         }
     }
+
+    private IEnumerator StopDashing()
+    {
+        yield return new WaitForSeconds(dashingTime);
+        trailRenderer.emitting = false;
+        isDashing = false;
+    }
+
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
